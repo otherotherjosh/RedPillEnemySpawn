6a3d3daf6ed817c3e8131256c45aef7df655c8c9
Josh <itsotherjosh@gmail.com>
2024-02-19 23:05:58 +1300

    Force all enemies out of enemy list and solely have red pill

--- Patches/RoundManagerPatch.cs
+++ Patches/RoundManagerPatch.cs
@@ -11,23 +11,35 @@ namespace RedPill.Patches
     {
         [HarmonyPatch(nameof(RoundManager.FinishGeneratingLevel))]
         [HarmonyPostfix]
-        static void ListAllEnemies()
+        static void AddRedPillToEnemiesList()
         {
+            EnemyType redPillEnemyType = null;
+            foreach (EnemyType e in Resources.FindObjectsOfTypeAll(typeof(EnemyType)))
+            {
+                if (e.name == "RedPillEnemyType")
+                {
+                    ModDebug.LogInfo($"Found {e.name}! Object: {e}");
+                    redPillEnemyType = e;
+                }
+            }
+
+            SpawnableEnemyWithRarity redPill = new SpawnableEnemyWithRarity();
+            if ( redPillEnemyType != null )
+            {
+                redPill.enemyType = redPillEnemyType;
+                redPill.rarity = 100;
+                RoundManager.Instance.currentLevel.Enemies = new List<SpawnableEnemyWithRarity>() { redPill };
+            }
+
+            #region list all enemies
             ModDebug.LogInfo("List of enemies on this level");
             foreach (SpawnableEnemyWithRarity e in RoundManager.Instance.currentLevel.Enemies)
             {
                 ModDebug.LogInfo($"(Rarity: {e.rarity}) {e.enemyType.name}");
             }
 
-            ModDebug.LogInfo("List of enemies found from FindObjects");
-            foreach (EnemyType e in Resources.FindObjectsOfTypeAll(typeof(EnemyType)))
-            {
-                //SpawnableEnemyWithRarity spawnable = new SpawnableEnemyWithRarity();
-                //spawnable.enemyType = e;
-                //ModDebug.LogInfo($" - {spawnable.enemyType.name}");
-
-                ModDebug.LogInfo($" - {e.name}");
-            }
+            //ModDebug.LogInfo("List of enemies found from FindObjects");
+            #endregion
         }
     }
 }
