1e62720099cd1f52916bedfeba1b0a85e45341f5
Josh <itsotherjosh@gmail.com>
2024-02-19 23:53:20 +1300

    Steal probability curve from baboon hawk (flatline at 1) and fix maxCount and rarity

--- Patches/RoundManagerPatch.cs
+++ Patches/RoundManagerPatch.cs
@@ -14,20 +14,40 @@ namespace RedPill.Patches
         static void AddRedPillToEnemiesList()
         {
             EnemyType redPillEnemyType = null;
+            AnimationCurve probabilityCurve = null;
+            
+            #region list all enemies
+            ModDebug.LogInfo("List of enemies on this level");
+            foreach (SpawnableEnemyWithRarity e in RoundManager.Instance.currentLevel.Enemies)
+            {
+                ModDebug.LogInfo($"(Rarity: {e.rarity}) {e.enemyType.name}");
+            }
+            #endregion
+
             foreach (EnemyType e in Resources.FindObjectsOfTypeAll(typeof(EnemyType)))
             {
+                if (e.name == "BaboonHawk")
+                {
+                    ModDebug.LogInfo($"Found {e.name}! Stealing probability curve...");
+                    probabilityCurve = e.probabilityCurve;
+
+                }
                 if (e.name == "RedPillEnemyType")
                 {
                     ModDebug.LogInfo($"Found {e.name}! Object: {e}");
+                    e.MaxCount = 5;
                     redPillEnemyType = e;
                 }
+                ModDebug.LogInfo($"{e.name}, max: {e.MaxCount}, curve: {e.probabilityCurve.length}, spawningDisabled: {e.spawningDisabled}\n" +
+                    $"power: {e.PowerLevel}");
             }
 
             SpawnableEnemyWithRarity redPill = new SpawnableEnemyWithRarity();
             if ( redPillEnemyType != null )
             {
                 redPill.enemyType = redPillEnemyType;
-                redPill.rarity = 100;
+                redPill.enemyType.probabilityCurve = probabilityCurve;
+                redPill.rarity = 58;
                 RoundManager.Instance.currentLevel.Enemies = new List<SpawnableEnemyWithRarity>() { redPill };
             }
 
@@ -37,8 +57,6 @@ namespace RedPill.Patches
             {
                 ModDebug.LogInfo($"(Rarity: {e.rarity}) {e.enemyType.name}");
             }
-
-            //ModDebug.LogInfo("List of enemies found from FindObjects");
             #endregion
         }
     }
