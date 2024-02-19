1fb71b94d07e83c9e61d76d78fa2191085887d6a
Josh <itsotherjosh@gmail.com>
2024-02-19 16:31:02 +1300

    Initialized project files

---
+++ Plugin.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace RedPill
+{
+    [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private const string PLUGIN_GUID = "NBFBs.RedPill";
+        private const string PLUGIN_NAME = "Red Pill Enemy Spawn";
+        private const string PLUGIN_VERSION = "0.1.0";
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PLUGIN_GUID} (\"{PLUGIN_NAME}\") is loaded!");
+        }
+    }
+}
